3e454fbc06af53418a9f682cfa6af70ebaa00212
JinyingJiang <jinyingjiang@foxmail.com>
2024-01-16 02:55:15 +0800

    jayden: update.

--- js/member.js
+++ js/member.js
@@ -1,3 +1,14 @@
+var member_black = [
+  {
+    "phone": "No.2038",
+    "name": "星野冥一"
+  },
+  {
+    "phone": "No.0282",
+    "name": "Mr.Q"
+  }
+]
+
 var member = [
   {
     "phone": "No.2038",
